b25ecc369bd69fc73e4d91d8743d9778d9e13bcd
Buppi Khan <khanbuppi@gmail.com>
2023-08-21 18:45:23 -0400

    added auth using clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <UserButton />
+    </div>
+  );
+};
+
+export default SetupPage;
