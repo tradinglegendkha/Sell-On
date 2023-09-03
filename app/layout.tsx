6fa1a5e3d0d7175fc27850d9c4caaaa2e1166124
Buppi Khan <khanbuppi@gmail.com>
2023-09-03 12:04:47 -0400

    added create store form, and reworked the sign in button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Clerk } from "@/node_modules/@clerk/nextjs/dist/types/server/clerkClient";
+import { ModalProvider } from "@/providers/modal-provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
