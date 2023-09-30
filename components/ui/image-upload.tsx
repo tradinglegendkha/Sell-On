771fb8e97d26e981f36ead06853421e4cf565379
Buppi Khan <khanbuppi@gmail.com>
2023-09-30 18:05:59 -0400

    implemented cloudinary for image uploading

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  return (
+    <div>
+      <div>ImageUpload</div>
+    </div>
+  );
+};
+
+export default ImageUpload;
