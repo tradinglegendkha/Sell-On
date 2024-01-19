23e296e214d017a8511169e333cbe2099c2f1e79
Buppi Khan <khanbuppi@gmail.com>
2024-01-19 18:47:32 -0500

    implemented image upload using cloudinary

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -20,17 +24,50 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   useEffect(() => {
     setIsMounted(true);
   }, []);
-  if (!isMounted) {
-    return null;
-  }
 
   const onUpload = (result: any) => {
     onChange(result.info.secure_url);
   };
 
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div>
-      <div>ImageUpload</div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="bnzahauc">
+        {({open }) => {
+          const onClick = () => {
+            open()
+          }
+          return (
+            <Button type="button" disabled={disabled} variant="secondary" onClick={onClick} >
+              <ImagePlus className="h-4 w-4 mr-2"/>
+              Upload an Image 
+            </Button>
+          )
+        }}
+      </CldUploadWidget>
     </div>
   );
 };
